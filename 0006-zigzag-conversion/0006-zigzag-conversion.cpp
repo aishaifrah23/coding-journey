d95136d4a0f7bb878b83dc72aca7146df8fc7aa5
aishaifrah23 <aishaifrah23@gmail.com>
2026-07-29 21:07:16 +0530

    Time: 7 ms (40.81%), Space: 14 MB (70.72%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        vector<string> rows(min(static_cast<int>(s.length()), numRows));
+        int currRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[currRow] += c;
+            if (currRow == 0 || currRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currRow += goingDown ? 1 : -1;
+        }
+
+        string result;
+        for (const string& row : rows) {
+            result += row;
+        }
+
+        return result;
+    }
+};
